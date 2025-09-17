d224b7c50e56805c92ca91714c561f74abab6a1e
Ephraim Nicolas <ephraim1588@gmail.com>
2025-09-16 21:24:27 -0400

    Configured localhost and vite api

--- frontend/src/pages/Predictor.jsx
+++ frontend/src/pages/Predictor.jsx
@@ -52,7 +52,7 @@ export default function Predictor() {
     if (!awayTeam || !homeTeam) return alert("Select both teams!");
 
     try {
-      const res = await fetch("http://127.0.0.1:5000/predict", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/predict`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ home: homeTeam, away: awayTeam }), // ✅ correct order
